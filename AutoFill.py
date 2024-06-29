64fd936eae961ace432f2792ea0ff8ccfb9eedf1
Novorsky <165407141+Novorsky@users.noreply.github.com>
2024-06-29 09:25:28 +0300

    Update AutoFill.py

--- AutoFill.py
+++ AutoFill.py
@@ -2,9 +2,9 @@ import keyboard
 
 
 def write_card_number():
-    keyboard.write("4441 1111 4173 3844")
+    keyboard.write("xxxx xxxx xxxx xxxx")
 
 
 while True:
     keyboard.wait("ctrl+alt+z+c")
-    keyboard.write("4441 1111 4173 3844")
+    keyboard.write("xxxx xxxx xxxx xxxx")
