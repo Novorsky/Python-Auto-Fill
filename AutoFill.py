a9d2d9d2f4ad03ae8c856f4765548d87361302f2
Novorsky <165407141+Novorsky@users.noreply.github.com>
2024-06-29 09:27:00 +0300

    AutoFill.py

--- AutoFill.py
+++ AutoFill.py
@@ -2,9 +2,9 @@ import keyboard
 
 
 def write_card_number():
-    keyboard.write("xxxx xxxx xxxx xxxx")
+    keyboard.write("xxxx xxxx xxxx xxxn")
 
 
 while True:
     keyboard.wait("ctrl+alt+z+c")
-    keyboard.write("xxxx xxxx xxxx xxxx")
+    keyboard.write("xxxx xxxx xxxx xxxn")
